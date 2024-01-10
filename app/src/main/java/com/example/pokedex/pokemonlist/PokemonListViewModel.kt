0e2bec8fba03b07b62eb1f7e31995a71e23ef451
DivisionCom <xd6511@gmail.com>
2024-01-10 23:05:29 +0300

    Add variable to ViewModel & Add const val PAGE_SIZE = 20

--- app/src/main/java/com/example/pokedex/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/example/pokedex/pokemonlist/PokemonListViewModel.kt
@@ -3,9 +3,11 @@ package com.example.pokedex.pokemonlist
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.palette.graphics.Palette
+import com.example.pokedex.data.models.PokedexListEntry
 import com.example.pokedex.repository.PokemonRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -15,6 +17,12 @@ class PokemonListViewModel @Inject constructor(
     private val repository: PokemonRepository
 ) : ViewModel() {
 
+    private var curPage = 0
+
+    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
 
     fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
         val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
