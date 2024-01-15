f24287ad9e299ab061e916a198ecac6b679c7067
DivisionCom <xd6511@gmail.com>
2024-01-15 23:55:36 +0300

    Add Searching Pokemon by name functionality

--- app/src/main/java/com/example/pokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedex/pokemonlist/PokemonListScreen.kt
@@ -56,7 +56,10 @@ import com.example.pokedex.data.models.PokedexListEntry
 import com.example.pokedex.ui.theme.RobotoCondensed
 
 @Composable
-fun PokemonListScreen(navController: NavController) {
+fun PokemonListScreen(
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltViewModel(),
+) {
     Surface(
         color = MaterialTheme.colorScheme.background,
         modifier = Modifier.fillMaxSize(),
@@ -78,6 +81,7 @@ fun PokemonListScreen(navController: NavController) {
                         .fillMaxWidth()
                         .padding(16.dp),
             ) {
+                viewModel.searchPokemonList(it)
             }
             Spacer(modifier = Modifier.height(16.dp))
             PokemonList(navController = navController)
@@ -147,6 +151,9 @@ fun PokemonList(
     val loadError by remember {
         viewModel.loadError
     }
+    val isSearching by remember {
+        viewModel.isSearching
+    }
 
     LazyColumn(contentPadding = PaddingValues(16.dp)) {
         val itemCount =
@@ -156,7 +163,7 @@ fun PokemonList(
                 pokemonList.size / 2 + 1
             }
         items(itemCount) {
-            if (it >= itemCount - 1 && !endReached && !isLoading) {
+            if (it >= itemCount - 1 && !endReached && !isLoading && !isSearching) {
                 viewModel.loadPokemonPaginated()
             }
             PokedexRow(rowIndex = it, entries = pokemonList, navController = navController)
